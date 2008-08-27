0739c406f414dce6ed987d833e7dd5610af825f5
grant <gaustin@gmail.com>
2008-08-27 02:17:56 +0100

    first commit

---
+++ feed_detector.rb
@@ -0,0 +1,51 @@
+require 'rubygems'
+require 'open-uri'
+require 'rfeedparser'
+
+class FeedDetector
+  ##
+  # return the feed url for a url
+  # for example: http://blog.dominiek.com/ => http://blog.dominiek.com/feed/atom.xml
+  # only_detect can force detection of :rss or :atom
+  # if nil is returned the has no discernible feed url -- perhaps because it's the feed url
+  def self.fetch_feed_url(page_url, only_detect=nil)
+    if page_url =~ /^http:\/\//
+      @html = open(page_url).read
+    else
+      @html = open("http://#{page_url}").read
+    end
+    
+    feed_url = self.get_feed_path(@html, only_detect)
+    feed_url = "http://#{host_with_port}/#{feed_url.gsub(/^\//, '')}" unless !feed_url || feed_url =~ /^http:\/\// 
+    feed_url
+  end
+
+  ##
+  # get the feed href from an HTML document
+  # for example:
+  # ...
+  # <link href="/feed/atom.xml" rel="alternate" type="application/atom+xml" />
+  # ...
+  # => /feed/atom.xml
+  # only_detect can force detection of :rss or :atom
+  def self.get_feed_path(html, only_detect=nil)
+    unless only_detect && only_detect != :atom
+      md ||= /<link.*href=['"]*([^\s'"]+)['"]*.*application\/atom\+xml.*>/.match(html) 
+      md ||= /<link.*application\/atom\+xml.*href=['"]*([^\s'"]+)['"]*.*>/.match(html) 
+    end
+    unless only_detect && only_detect != :rss
+      md ||= /<link.*href=['"]*([^\s'"]+)['"]*.*application\/rss\+xml.*>/.match(html) 
+      md ||= /<link.*application\/rss\+xml.*href=['"]*([^\s'"]+)['"]*.*>/.match(html) 
+    end
+    md && md[1]
+  end
+
+  def self.detect(url, only_detect=nil)
+    feed_url = self.fetch_feed_url(url, only_detect)
+    if feed_url.nil?
+      feed = FeedParser.parse(@html)
+      feed_url = feed.url if feed.version =~ /^#{only_detect}\d*/ 
+    end
+    feed_url
+  end
+end
---
+++ feed_detector_test.rb
@@ -0,0 +1,78 @@
+require 'test/unit'
+require 'feed_detector'
+
+class FeedDetectorTest < Test::Unit::TestCase
+  def setup
+    @body = []
+    make_default_body
+    @default_feed_url = 'http://giftedslacker.com/feed/'
+    @default_page_url = 'http://giftedslacker.com/'
+  end
+  
+  def test_wordpress_detect
+    # page containing a feed pointer
+    feed_path = FeedDetector.detect(@default_page_url)
+    assert_equal(@default_feed_url, feed_path)
+    feed_path = FeedDetector.detect(@default_page_url, :rss)
+    assert_equal(@default_feed_url, feed_path)
+    feed_path = FeedDetector.detect(@default_page_url, :atom)
+    assert_equal(nil, feed_path)
+    
+    # the feed itself
+    feed_path = FeedDetector.detect(@default_feed_url)
+    assert_equal(@default_feed_url, feed_path)
+    feed_path = FeedDetector.detect(@default_feed_url, :atom)
+    assert_equal(@default_feed_url, feed_path)
+    feed_path = FeedDetector.detect(@default_feed_url, :rss)
+    assert_equal(nil, feed_path)
+  end
+  def test_wordpress_only_detect
+    make_wordpress_body
+    feed_path = FeedDetector.get_feed_path(@body.join("\n"), :atom)
+    assert_equal(nil, feed_path)
+    feed_path = FeedDetector.get_feed_path(@body.join("\n"), :rss)
+    assert_equal(@default_feed_url, feed_path)
+  end
+  
+  def test_wordpress
+    make_wordpress_body
+    feed_path = FeedDetector.get_feed_path(@body.join("\n"))   
+    assert_equal(@default_feed_url, feed_path)
+  end
+  
+  def test_wordpress_only_detect_net
+    feed_path = FeedDetector.fetch_feed_url(@default_page_url, :atom)
+    assert_equal(nil, feed_path)
+    feed_path = FeedDetector.fetch_feed_url(@default_page_url, :rss)
+    assert_equal(@default_feed_url, feed_path)
+  end
+  
+  def test_wordpress_net
+    feed_path = FeedDetector.fetch_feed_url(@default_page_url)
+    assert_equal(@default_feed_url, feed_path)
+  end
+  
+  #TODO: add tests for malformed urls and pages without a feed
+  
+private
+
+  def make_wordpress_body
+    @body = []
+    @body << ' <html>'
+    @body << '  <head>'
+    @body << '   <link href="/super.css" rel="alternate" type="text/css"/>'
+    @body << '   <link rel="alternate" type="application/rss+xml" title="Gifted Slacker RSS Feed" href="http://giftedslacker.com/feed/" />'
+    @body << '  </head>'
+    @body << ' </html>'
+  end
+  
+  def make_default_body
+    @body = []
+    @body << ' <html>'
+    @body << '  <head>'
+    @body << '   <link href="/super.css" rel="alternate" type="text/css"/>'
+    @body << '   <link href="/feed/atom.xml" rel="alternate" type="application/atom+xml" />'
+    @body << '  </head>'
+    @body << ' </html>'
+  end
+end
