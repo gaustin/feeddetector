637dca3ff6949d26a3e449f1ffbea5656d2d1f2d
grant <gaustin@gmail.com>
2008-08-31 18:02:59 +0100

    refactored default html generator for tests and removed some irrelevant tests

--- feed_detector_test.rb
+++ feed_detector_test.rb
@@ -17,17 +17,10 @@ class FeedDetectorTest < Test::Unit::TestCase
     assert_equal(@default_feed_url, feed_path)
     feed_path = FeedDetector.detect(@default_page_url, :atom)
     assert_equal(nil, feed_path)
-    
-    # the feed itself
-    feed_path = FeedDetector.detect(@default_feed_url)
-    assert_equal(@default_feed_url, feed_path)
-    feed_path = FeedDetector.detect(@default_feed_url, :atom)
-    assert_equal(@default_feed_url, feed_path)
-    feed_path = FeedDetector.detect(@default_feed_url, :rss)
-    assert_equal(nil, feed_path)
   end
+  
   def test_wordpress_only_detect
-    make_wordpress_html
+    make_head(make_wordpress_html)
     feed_path = FeedDetector.get_feed_path(@body.join("\n"), :atom)
     assert_equal(nil, feed_path)
     feed_path = FeedDetector.get_feed_path(@body.join("\n"), :rss)
@@ -35,7 +28,7 @@ class FeedDetectorTest < Test::Unit::TestCase
   end
   
   def test_wordpress
-    make_wordpress_html
+    make_head(make_wordpress_html)
     feed_path = FeedDetector.get_feed_path(@body.join("\n"))   
     assert_equal(@default_feed_url, feed_path)
   end
@@ -56,12 +49,26 @@ class FeedDetectorTest < Test::Unit::TestCase
   
 private
 
+  def make_blogger_html
+    body = []
+    body << '<link rel="alternate" type="application/atom+xml" title="draw this: hats - Atom" href="http://drawthis.blogspot.com/feeds/posts/default" />'
+    body << '<link rel="alternate" type="application/rss+xml" title="draw this: hats - RSS" href="http://drawthis.blogspot.com/feeds/posts/default?alt=rss" />'
+    body << '<link rel="service.post" type="application/atom+xml" title="draw this: hats - Atom" href="http://www.blogger.com/feeds/2321821791771150656/posts/default" />'
+    body
+  end
+  
   def make_wordpress_html
+    body = []
+    body << '   <link href="/super.css" rel="alternate" type="text/css"/>'
+    body << '   <link rel="alternate" type="application/rss+xml" title="Gifted Slacker RSS Feed" href="http://giftedslacker.com/feed/" />'
+    body
+  end
+  
+  def make_head(lines)
     @body = []
     @body << ' <html>'
     @body << '  <head>'
-    @body << '   <link href="/super.css" rel="alternate" type="text/css"/>'
-    @body << '   <link rel="alternate" type="application/rss+xml" title="Gifted Slacker RSS Feed" href="http://giftedslacker.com/feed/" />'
+    lines.each { |line| @body << line }
     @body << '  </head>'
     @body << ' </html>'
   end
