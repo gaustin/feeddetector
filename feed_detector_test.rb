34170095f59ec6057aaa4cb35ae03377fbf1a359
gaustin <gaustin@gmail.com>
2008-09-17 15:23:19 -0400

    updated tests to reflect api changes

--- feed_detector_test.rb
+++ feed_detector_test.rb
@@ -22,17 +22,22 @@ class FeedDetectorTest < Test::Unit::TestCase
   def test_get_feed_path
     # page containing no feeds
     html = make_html(nil_feed_html).join("\n")
-    feed_paths = FeedDetector.get_feed_path(html)
+    feed_paths = FeedDetector.get_feed_paths(html)
     assert_equal([], feed_paths)
     
-    # page containing a single feed
-    html = make_html(single_feed_html).join("\n")
-    feed_paths = FeedDetector.get_feed_path(html)
+    # page containing a single atom feed
+    html = make_html(single_atom_feed_html).join("\n")
+    feed_paths = FeedDetector.get_feed_paths(html)
     assert_equal(['/feed/atom.xml'], feed_paths)
+    
+    # page containing a single rss feed
+    html = make_html(single_rss_feed_html).join("\n")
+    feed_paths = FeedDetector.get_feed_paths(html)
+    assert_equal(['/feed/blog.xml'], feed_paths)
 
     # page containing several feeds
     html = make_html(multi_feed_html).join("\n")
-    feed_paths = FeedDetector.get_feed_path(html)    
+    feed_paths = FeedDetector.get_feed_paths(html)    
     assert_equal(["http://ethandraws.blogspot.com/feeds/posts/default",
                   "http://www.blogger.com/feeds/21351008/posts/default",
                   "http://ethandraws.blogspot.com/feeds/posts/default?alt=rss",
@@ -40,22 +45,58 @@ class FeedDetectorTest < Test::Unit::TestCase
                   "/feed/atom.xml"], feed_paths)    
   end
   
-  def test_fetch_feed_url
+  def test_get_feed_paths_with_only_detect
+    # page containing no feeds w/rss
+    html = make_html(nil_feed_html).join("\n")
+    feed_paths = FeedDetector.get_feed_paths(html, :rss)
+    assert_equal([], feed_paths)
+    
+    # page containing no feeds w/atom
+    html = make_html(nil_feed_html).join("\n")
+    feed_paths = FeedDetector.get_feed_paths(html, :atom)
+    assert_equal([], feed_paths)
+    
+    # page containing a single feed w/rss
+    html = make_html(single_rss_feed_html).join("\n")
+    feed_paths = FeedDetector.get_feed_paths(html, :rss)
+    assert_equal(['/feed/blog.xml'], feed_paths)
+    
+    # page containing a single feed w/atom
+    html = make_html(single_atom_feed_html).join("\n")
+    feed_paths = FeedDetector.get_feed_paths(html, :atom)
+    assert_equal(['/feed/atom.xml'], feed_paths)
+
+    # page containing several feeds w/rss
+    html = make_html(multi_feed_html(:rss)).join("\n")
+    feed_paths = FeedDetector.get_feed_paths(html, :rss)    
+    assert_equal(["http://ethandraws.blogspot.com/feeds/posts/default?alt=rss",
+                  "http://giftedslacker.com/feed/",
+                  "/feed/blog.xml"], feed_paths)
+                  
+    # page containing several feeds w/atom
+    html = make_html(multi_feed_html(:atom)).join("\n")
+    feed_paths = FeedDetector.get_feed_paths(html, :atom)    
+    assert_equal(["http://ethandraws.blogspot.com/feeds/posts/default",
+                  "http://www.blogger.com/feeds/21351008/posts/default",
+                  "/feed/atom.xml"], feed_paths)
+  end
+  
+  def test_fetch_feed_urls
     #page containing a single feed pointer
-    feed_paths = FeedDetector.fetch_feed_url(@wordpress_single_feed_page_url)
+    feed_paths = FeedDetector.fetch_feed_urls(@wordpress_single_feed_page_url)
     assert_equal([@wordpress_atom_url], feed_paths)
-    feed_paths = FeedDetector.fetch_feed_url(@wordpress_single_feed_page_url, :rss)
+    feed_paths = FeedDetector.fetch_feed_urls(@wordpress_single_feed_page_url, :rss)
     assert_equal([@wordpress_atom_url], feed_paths)
-    feed_paths = FeedDetector.fetch_feed_url(@wordpress_single_feed_page_url, :atom)
+    feed_paths = FeedDetector.fetch_feed_urls(@wordpress_single_feed_page_url, :atom)
     assert_equal([], feed_paths)
     
     #page containing several feed pointers    
-    feed_paths = FeedDetector.fetch_feed_url(@wordpress_several_feed_page_url)
+    feed_paths = FeedDetector.fetch_feed_urls(@wordpress_several_feed_page_url)
     assert_equal(["http://www.hasmanydevelopers.com/atom.xml",
                   "http://www.hasmanydevelopers.com/rss.xml"], feed_paths)
-    feed_paths = FeedDetector.fetch_feed_url(@wordpress_several_feed_page_url, :rss)
+    feed_paths = FeedDetector.fetch_feed_urls(@wordpress_several_feed_page_url, :rss)
     assert_equal(["http://www.hasmanydevelopers.com/rss.xml"], feed_paths)
-    feed_paths = FeedDetector.fetch_feed_url(@wordpress_several_feed_page_url, :atom)
+    feed_paths = FeedDetector.fetch_feed_urls(@wordpress_several_feed_page_url, :atom)
     assert_equal(["http://www.hasmanydevelopers.com/atom.xml"], feed_paths)
   end
   
@@ -107,19 +148,29 @@ class FeedDetectorTest < Test::Unit::TestCase
   #TODO: add tests for malformed urls and pages without a feed
 
 private  
-  def multi_feed_html
+  def multi_feed_html(spec)
     body = []
     body << '   <link rel="alternate" type="application/atom+xml" title="Ethan Draws - Atom" href="http://ethandraws.blogspot.com/feeds/posts/default" />'
     body << '   <link rel="service.post" type="application/atom+xml" title="Ethan Draws - Atom" href="http://www.blogger.com/feeds/21351008/posts/default" />'    
     body << '   <link rel="alternate" type="application/rss+xml" title="Ethan Draws - RSS" href="http://ethandraws.blogspot.com/feeds/posts/default?alt=rss" />'
     body << '   <link rel="alternate" type="application/rss+xml" title="Gifted Slacker RSS Feed" href="http://giftedslacker.com/feed/" />'
+    if spec == :rss
+      body << '   <link href="/feed/blog.xml" rel="alternate" type="application/rss+xml" />'
+    else
+      body << '   <link href="/feed/atom.xml" rel="alternate" type="application/atom+xml" />'    
+    end
+    body
+  end
+  
+  def single_atom_feed_html
+    body = []
     body << '   <link href="/feed/atom.xml" rel="alternate" type="application/atom+xml" />'
     body
   end
   
-  def single_feed_html
+  def single_rss_feed_html
     body = []
-    body << '   <link href="/feed/atom.xml" rel="alternate" type="application/atom+xml" />'
+    body << '   <link href="/feed/blog.xml" rel="alternate" type="application/rss+xml" />'
     body
   end
   
